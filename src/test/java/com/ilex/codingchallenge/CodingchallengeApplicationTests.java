43624ad10ca8c7cdad2da855cbabe05c369af4f5
Vincent Geng <vincentgeng90@gmail.com>
2020-12-18 23:12:06 +0800

    first commit

---
+++ src/test/java/com/ilex/codingchallenge/CodingchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilex.codingchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
