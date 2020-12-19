8ebc4d1579ab27b7466385dfc759ef295552f8c0
Vincent Geng <vincentgeng90@gmail.com>
2020-12-19 11:31:23 +0800

    Add annotation and dependencies for controllers

--- src/main/java/com/ilex/codingchallenge/order/controller/OrderController.java
+++ src/main/java/com/ilex/codingchallenge/order/controller/OrderController.java
@@ -1,10 +1,20 @@
 package com.ilex.codingchallenge.order.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ilex.codingchallenge.order.service.OrderService;
+
 /**
  * @author Vincent Geng
  *
  * Created on Dec 18, 2020
  */
+
+@RestController
 public class OrderController {
+	
+	@Autowired
+	private OrderService orderService;
 
 }
--- src/main/java/com/ilex/codingchallenge/product/controller/ProductController.java
+++ src/main/java/com/ilex/codingchallenge/product/controller/ProductController.java
@@ -1,10 +1,20 @@
 package com.ilex.codingchallenge.product.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ilex.codingchallenge.product.service.ProductService;
+
 /**
  * @author Vincent Geng
  *
  * Created on Dec 18, 2020
  */
+
+@RestController
 public class ProductController {
+	
+	@Autowired
+	private ProductService productService;
 
 }
